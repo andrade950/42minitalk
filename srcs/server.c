4e78fdaf9f422d1e4571cb267ab2d13105828b57
andrade950 <joao2005andrade@gmail.com>
2025-02-28 19:21:32 +0000

    Update server.c

--- srcs/server.c
+++ srcs/server.c
@@ -3,14 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   server.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: joaomart <joaomart@student.42.fr>          +#+  +:+       +#+        */
+/*   By: andrade <andrade@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 09:55:47 by joaomart          #+#    #+#             */
-/*   Updated: 2025/02/26 21:18:58 by joaomart         ###   ########.fr       */
+/*   Updated: 2025/02/28 19:19:27 by andrade          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minitalk.h"
+
+static char	*full_str = NULL;
+static char	*bits = NULL;
+
+static void	handle_exit(int signum)
+{
+	(void)signum;
+	if (full_str)
+		free(full_str);
+	if (bits)
+		free(bits);
+	exit(0);
+}
 
 static unsigned char	bin_to_ascii(char *str)
 {
@@ -49,26 +62,24 @@ static char	*append_c(char *start, char c)
 	return (temp);
 }
 
-static void	print_str(char **bits, char **full_str)
+static void	print_str()
 {
 	char	c;
 
-	c = bin_to_ascii(*bits);
-	free(*bits);
-	*bits = NULL;
-	*full_str = append_c(*full_str, c);
+	c = bin_to_ascii(bits);
+	free(bits);
+	bits = NULL;
+	full_str = append_c(full_str, c);
 	if (c == '\0')
 	{
-		ft_putendl_fd(*full_str, 1);
-		free(*full_str);
-		*full_str = NULL;
+		ft_putendl_fd(full_str, 1);
+		free(full_str);
+		full_str = NULL;
 	}
 }
 
 static void	append_bit(int signum)
 {
-	static char	*full_str = NULL;
-	static char	*bits;
 	static int	bit_count;
 
 	bit_count++;
@@ -84,7 +95,7 @@ static void	append_bit(int signum)
 	else
 		bits = append_c(bits, '1');
 	if (bit_count == 8)
-		print_str(&bits, &full_str);
+		print_str();
 }
 
 int	main(void)
@@ -92,7 +103,7 @@ int	main(void)
 	ft_printf("Server PID: %u\n", getpid());
 	signal(SIGUSR1, append_bit);
 	signal(SIGUSR2, append_bit);
-	signal(SIGINT, exit);
+	signal(SIGINT, handle_exit);
 	while (1)
 		pause();
 	return (0);
